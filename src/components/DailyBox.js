7404e8276d2128d0908bd2dabd28defee956d3dd
barbaradyguda <41574313+barbaradyguda@users.noreply.github.com>
2022-08-01 20:52:19 +0200

    data for every day

--- src/components/DailyBox.js
+++ src/components/DailyBox.js
@@ -1,31 +1,47 @@
 import React, { useEffect, useState } from "react";
 import { styles } from "./styles"
-import { Box, Typography } from "@mui/material"
+import { Box, Typography, Button } from "@mui/material"
 
 
-const DailyBox = ({data}) => {
+const DailyBox = ({
+    visibleDay,
+    setVisibleDay,
+    dayTemperatures,
+    nightTemperatures,
+    morningTemperature,
+    humidity,
+    minTempValues,
+    maxTempValues
 
-    const dailyTemperature = (data.temp - 272.15).toFixed(2);
-    const nightlyTemperature = (data.temp - 272.15).toFixed(2);
-    const humidity = data.humidity
+}) => {
+
+    // const dailyTemperature = (data.temp - 272.15).toFixed(2);
+    // const nightlyTemperature = (data.temp - 272.15).toFixed(2);
+    // const humidity = data.humidity
 
     useEffect(() => {
         console.debug("wherearewenow")
     }, []);
 
+
+    const getMeanDailyTemperature = (dayTemperatures) => {
+        // console.debug("---------dayTemperatures", dayTemperatures)
+        // const sum = dayTemperatures.reduce((a, b) => a + b, 0);
+        // console.debug("sum", sum)
+        // const meanDailyTemperature = (sum / dayTemperatures.length) || 0;
+        // console.debug("meanDailyTemperature", meanDailyTemperature)
+        // return meanDailyTemperature
+    }
+
     // const arrayDailyData = dailyData && Object.entries(dailyData);
 
     return (
-        <Box sx={styles.searchContainer} style={{ height: 10, backgroundColor: "blue", margin: 8}}>
-    <Typography>
-        {dailyTemperature + "C"}
-    </Typography>
-    <Typography>
-        {dailyTemperature + "C"}
-    </Typography>
-    <Typography>
-        {humidity + "%"}
-    </Typography>
+        <Box sx={styles.searchContainer} style={{ height: 10, backgroundColor: "blue", margin: 8 }}>
+            <Button onClick={()=>setVisibleDay("2022-08-02")}>
+               { console.debug(visibleDay)}
+                {/* <Typography>{getMeanDailyTemperature(dayTemperatures)}</Typography>
+                <Typography>{getMeanDailyTemperature(dayTemperatures)}</Typography> */}
+            </Button>
         </Box>
     );
 }
--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,23 +1,43 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Search"
 import Chart from "./Chart"
-import Days from "./Days"
+import DailyBox from "./DailyBox"
 import axios from "axios";
 import { Box } from "@mui/material"
 import { styles } from "./styles";
 
 const WeatherForecast = () => {
     const [weatherData, setWeatherData] = useState([]);
-    const [city, setCity] = useState("London");
+    const [city, setCity] = useState(" ");
     const apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=";
-    const appid = "d478c88b2fe7099efacc54439530b9d3";
+    const appid = "1a9103af69a24a48d5f58a8d0592d754";
     const timestamps = 40; //in free demo version available is only "3-hour Forecast 5 days" option :) so I need to get 40 timestamps to get data about 5 days
-    const [dailyData, setDailyData] = useState();
-    const [dayOne, setDayOne] = useState();
-    const [dayTwo, setDayTwo] = useState();
-    const [dayThree, setDayThree] = useState();
-    const [dayFour, setDayFour] = useState();
-    const [dayFive, setDayFive] = useState();
+
+    let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+    let localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+    const [visibleDay, setVisibleDay] = useState(localISOTime.split('T')[0]);
+
+    const [dayTemperatures, setDayTemperatures] = useState([]);
+    const [dayTemperature, setDayTemperature] = useState();
+    const [nightTemperatures, setNightTemperatures] = useState([]);
+    const [morningTemperature, setMorningTemperature] = useState(" ");
+    const [humidity, setHumidity] = useState([]);
+    const [minTempValues, setMinTempValues] = useState([]);
+    const [maxTempValues, setMaxTempValues] = useState([]);
+
+
+    // const tomorrow = new Date();
+    // tomorrow.setDate(tomorrow.getDate() + 1);
+    // console.log("tomorrow",tomorrow)
+
+    useEffect(() => {
+        setCity("London")
+    }, []);
+
+    useEffect(() => {
+        getData();
+    }, [city, visibleDay]);
+
 
     const getData = async () => {
         await axios
@@ -25,38 +45,97 @@ const WeatherForecast = () => {
             .then(async (response) => {
                 const weatherData = response.data;
                 setWeatherData(weatherData);
-                getDailyData(weatherData)
+                getDailyData(weatherData);
             })
             .catch((err) => console.debug(err));
     };
 
-    useEffect(() => {
-        getData();
-    }, [city]);
 
     const getDailyData = (weatherData) => {
         const dailiesData = weatherData.list
-        setDailyData(dailiesData.reduce((acc, day) => {
-            const date = day.dt_txt.split(' ')[0];
-            console.debug("day", day)
-            if (acc[date]) {
-                acc[date].temp_max = Math.max(acc[date].temp_max, day.main.temp_max);
-                acc[date].temp_min = Math.min(acc[date].temp_min, day.main.temp_min);
-                acc[date].humidity = day.main.humidity;
-                acc[date].temp = day.main.temp;
-            } else {
-                acc[date] = { temp_max: day.main.temp_max, temp_min: day.main.temp_min, humidity: day.main.humidity, temp: day.main.temp };
-            }
-            return acc;
-        }, {}));
-        return dailyData
+        console.debug("dailiesData", dailiesData)
+
+        dailiesData && dailiesData
+            .filter((dailyData) => dailyData.dt_txt.split(' ')[0] === visibleDay)
+            .map((dailyData, index) => {
+                let time = dailyData.dt_txt.split(' ')[1]
+                let date = dailyData.dt_txt.split(' ')[0]
+                let hum = dailyData.main.humidity
+                let temp = (dailyData.main.temp - 272.15).toFixed(1)
+                let maxTemp = (dailyData.main.temp_max - 272.15).toFixed(1)
+                let minTemp = (dailyData.main.temp_min - 272.15).toFixed(1)
+
+                console.debug("dailyData", dailyData)
+
+                setVisibleDay(date)
+                setHumidity(Object.entries(humidity.push(hum)));
+
+                if (time === "09:00:00" || time === "12:00:00" || time === "15:00:00" || time === "18:00:00") {
+                    setDayTemperatures(Object.entries(dayTemperatures.push(temp)))
+
+                } else if (time === "21:00:00" || time === "00:00:00" || time === "03:00:00") {
+
+                    setNightTemperatures(Object.entries(nightTemperatures.push(temp)))
+                } else if (time === "06:00:00") {
+                    setMorningTemperature(temp)
+                }
+
+                setMinTempValues(Object.entries(minTempValues.push(minTemp)))
+                setMaxTempValues(Object.entries(maxTempValues.push(maxTemp)))
+
+                //  console.debug("average", getAverage(dayTemperatures))
+
+
+            })
+
+
+        // console.debug("visibleday", visibleDay)
+        // console.debug("controll humidity", humidity)
+        // console.debug("csetMorningTemperatureity", morningTemperature)
+        console.debug("controll dayTemperatures", dayTemperatures)
+        
+const arrOfNum = dayTemperatures.map(str => {
+    return Number(str);
+  });
+        const sum =  arrOfNum.reduce((a, b) => a + b, 0);
+        const meanDailyTemperature = (sum / arrOfNum.length) || 0;
+        setDayTemperature(meanDailyTemperature)
+        console.debug("meanDailyTemperature", meanDailyTemperature)
+
     }
+
+
+    const getAverage = (dayTemperatures) => {
+        console.debug("---------dayTemperatures", dayTemperatures)
+        const sum = dayTemperatures.reduce((a, b) => a + b, 0);
+        console.debug("sum", sum)
+        const meanDailyTemperature = (sum / dayTemperatures.length) || 0;
+        console.debug("meanDailyTemperature", meanDailyTemperature)
+        return meanDailyTemperature
+    }
+
+
 
     return (
         <Box sx={styles.container}>
             <Search city={city} setCity={setCity} />
             <Chart />
-            {dailyData && <Days dailyData={dailyData} />}
+            {console.debug("meanDailyyyyyyyyyyyyyyyyyyTemperature",dayTemperature)}
+            {/* {console.debug("222visibleday", visibleDay)}
+            {console.debug("222controll humidity", humidity)}
+            {console.debug("222csetMorningTemperatureity", morningTemperature)}
+            {console.debug("222controll dayTemperatures", dayTemperatures)} */}
+            {weatherData &&
+                <DailyBox
+                    visibleDay={visibleDay}
+                    setVisibleDay={setVisibleDay}
+                    dayTemperatures={dayTemperatures}
+                    nightTemperatures={nightTemperatures}
+                    morningTemperature={morningTemperature}
+                    humidity={humidity}
+                    minTempValues={minTempValues}
+                    maxTempValues={maxTempValues}
+                />}
         </Box>
     );
 };
